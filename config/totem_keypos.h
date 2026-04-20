30b9c8be37a0dda50245b188934e414affe09986
Kirill Kovalchuk <kkovalchuk@atlassian.com>
2026-04-20 16:07:51 +1000

    fix(totem): move pinky keys to bottom row where they physically are
    TOTEM matrix has extra pinky keys (pos 20, 31) on the bottom/Z row,
    not the middle/home row. Was causing all keys on rows 2-3 to be
    shifted by one position.

--- config/totem_keypos.h
+++ config/totem_keypos.h
@@ -1,15 +1,15 @@
 /*
  * Key position defines for TOTEM (38 keys)
  *
- *      ╭───┬───┬───┬───┬───╮       ╭───┬───┬───┬───┬───╮
- *      │ 0 │ 1 │ 2 │ 3 │ 4 │       │ 5 │ 6 │ 7 │ 8 │ 9 │
- * ╭────┼───┼───┼───┼───┼───┼       ├───┼───┼───┼───┼───┼────╮
- * │ 20 │10 │11 │12 │13 │14 │       │15 │16 │17 │18 │19 │ 31 │
- * ╰────┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼────╯
- *      │21 │22 │23 │24 │25 │       │26 │27 │28 │29 │30 │
- *      ╰───┴───┼───┼───┼───┤       ├───┼───┼───┼───┴───╯
- *              │32 │33 │34 │       │35 │36 │37 │
- *              ╰───┴───┴───╯       ╰───┴───┴───╯
+ *           ╭───┬───┬───┬───┬───╮       ╭───┬───┬───┬───┬───╮
+ *           │ 0 │ 1 │ 2 │ 3 │ 4 │       │ 5 │ 6 │ 7 │ 8 │ 9 │
+ *           ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+ *           │10 │11 │12 │13 │14 │       │15 │16 │17 │18 │19 │
+ * ╭────┬────┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼────┬────╮
+ * │ 20 │ 21 │22 │23 │24 │25 │       │26 │27 │28 │29 │30 │ 31 │
+ * ╰────┴────┴───┼───┼───┼───╯       ╰───┼───┼───┼───┴───┴────╯
+ *               │32 │33 │34 │       │35 │36 │37 │
+ *               ╰───┴───┴───╯       ╰───┴───┴───╯
  */
 
 #pragma once
@@ -26,8 +26,7 @@
 #define RT3  8
 #define RT4  9
 
-#define LM5 20 // Left-Middle (ASDFGH) row — extra pinky
-#define LM4 10
+#define LM4 10 // Left-Middle (ASDFGH) row
 #define LM3 11
 #define LM2 12
 #define LM1 13
@@ -38,9 +37,9 @@
 #define RM2 17
 #define RM3 18
 #define RM4 19
-#define RM5 31 // Right extra pinky
 
-#define LB4 21 // Left-Bottom (ZXCVBN) row
+#define LB5 20 // Left-Bottom (ZXCVBN) row — extra pinky
+#define LB4 21
 #define LB3 22
 #define LB2 23
 #define LB1 24
@@ -51,6 +50,7 @@
 #define RB2 28
 #define RB3 29
 #define RB4 30
+#define RB5 31 // Right extra pinky
 
 #define LH2 32 // Left Thumb Cluster
 #define LH1 33
@@ -60,6 +60,6 @@
 #define RH1 36
 #define RH2 37
 
-#define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LM5 LB0 LB1 LB2 LB3 LB4
-#define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RM5 RB0 RB1 RB2 RB3 RB4
+#define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4 LB5
+#define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4 RB5
 #define THUMBS LH0 LH1 LH2 RH0 RH1 RH2
